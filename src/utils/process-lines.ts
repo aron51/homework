f3c65324dd7119826658f26d65c49ae4640f4653
Aron Szabo <aron.szabo@ge.com>
2019-10-09 17:43:21 +0200

    create constants file

--- src/utils/process-lines.ts
+++ src/utils/process-lines.ts
@@ -1,20 +1,24 @@
+import { SEPARATOR } from './constants';
 import { Node } from '../models';
 
 export const createNodeArrayFromLines = (lines: string[]): Node[] => {
     const nodes = new Map<string, Node>();
 
     lines.forEach(line => {
-        const label = line.split('=>')[0].trim();
+        const label = line.split(SEPARATOR)[0].trim();
+        
         nodes.set(label, new Node(label));
     })
 
     lines.forEach(line => {
-        const splitted =  line.split('=>');
+        const splitted =  line.split(SEPARATOR);
         const label = splitted[0].trim();
         const dependency = splitted[1].trim();
+
         if (dependency) {
             const node = nodes.get(label);
             const neighbour = nodes.get(dependency);
+
             node.addNeighbour(neighbour);
 
             nodes.set(label, node);
